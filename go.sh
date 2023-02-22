6283ff730908fe7928f269c95546f28a8d6b7296
scruffy <yishuiwang163@gmail.com>
2023-02-22 09:58:59 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -5,3 +5,4 @@ tar -zxvf go1.19.2.linux-amd64.tar.gz
 echo -e "export GOROOT=/usr/local/go\nexport GOPATH=\$HOME\nexport PATH=\$PATH:\$GOROOT/bin:\$GPPATH/bin" >> /etc/profile
 source /etc/profile
 go version
+source /etc/profile
